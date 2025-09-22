afa43fa0b232327e18d01962e737a032af12cf1a
nik-1718y <162183700+nik-1718y@users.noreply.github.com>
2025-09-22 14:03:51 +0530

    first

--- Backend/jwt/AuthToken.js
+++ Backend/jwt/AuthToken.js
@@ -7,7 +7,7 @@ const createTokenAndSaveCookies =async(userId,res)=>{
     expiresIn:"7d"
   })
   res.cookie("jwt",token,{
-     httpOnly:false,
+     httpOnly:true,
      secure:true,
      sameSite:"none",
   });
