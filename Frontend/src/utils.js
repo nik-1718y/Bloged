438338a948069fafcdd8d675a6f0363ec2d9ab90
nik-1718y <162183700+nik-1718y@users.noreply.github.com>
2025-09-23 20:51:26 +0530

    done

--- Frontend/src/utils.js
+++ Frontend/src/utils.js
@@ -1,2 +1,2 @@
-// export const BACKEND_URL="https://bloged-1.onrender.com"
-export const BACKEND_URL="https://localhost:4001"
+export const BACKEND_URL="https://bloged-11.onrender.com"
+// export const BACKEND_URL="https://localhost:4001"
